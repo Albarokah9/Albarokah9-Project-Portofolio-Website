f6fe081912fb09b9f8c37096c5550a0922ecff36
Albarokah9 <albarokahrifansahsutantoputra9@gmail.com>
2025-12-09 19:26:21 +0700

    feat: configure Vite for GitHub Pages deployment and set up workflow for automated deployment

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: './', // Makes asset paths relative, helping with deployment to subdirectories
+  base: '/Albarokah9-Project-Portofolio-Website/',  // Sesuaikan dengan nama repository
+  server: {
+    port: 5173
+  }
 })
