8a142546b44ebb6eb86ab1246559c374ec68935c
Albarokah9 <albarokahrifansahsutantoputra9@gmail.com>
2025-12-09 19:30:59 +0700

    fix: update deployment workflow to clean install dependencies and upload build artifacts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Albarokah9-Project-Portofolio-Website/',  // Sesuaikan dengan nama repository
+  base: '/Albarokah9-Project-Portofolio-Website/',
   server: {
     port: 5173
   }
